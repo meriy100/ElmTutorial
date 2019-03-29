008e972d1589ad1a5a1e060ed8ae68c25783d080
meriy100 <ttattataa@gmail.com>
2019-03-29 20:10:35 +0900

    [Add] http test

--- src/HttpTest.elm
+++ src/HttpTest.elm
@@ -1,4 +1,5 @@
-module Http exposing (..)
+module HttpTest exposing (..)
+import Browser
 import Html exposing (Html, text, pre)
 import Http
 
@@ -14,7 +15,38 @@ init : () -> (Model, Cmd Msg)
 init _ =
     ( Loading
     , Http.get
-        { url = "http://localhost:9090/output.txt"
+        { url = "http://localhost:4000"
         , expect = Http.expectString GotText
         }
     )
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg _ =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    (Success fullText, Cmd.none)
+                Err _ ->
+                    (Failure, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load your book."
+        Loading ->
+            text "Loading..."
+        Success fullText ->
+            pre [] [ text fullText ]
+
+main =
+    Browser.element
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
